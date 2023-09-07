443839c56db173b966a9a3db155432349588a1d5
peenu <ivo.valiste@gmail.com>
2023-09-07 19:13:25 +0300

    new code

--- ver1/main.go
+++ ver1/main.go
@@ -14,29 +14,39 @@ import (
 	"github.com/Pallinder/go-randomdata"
 )
 
-// üks töögrupp kõik teevad sama asja. loob kasutaja saab id ja teeb 3 kaarti
-func main() {
-	apiEp := getenvs.GetEnvString("APIURL", "http://localhost:8080")
-	userCount, _ := getenvs.GetEnvInt("USERCOUNT", 10000) // Total number of users to create( user = 1 user ja 3 kaarti)
-	workerCount, _ := getenvs.GetEnvInt("WORKERCOUNT", 4) // Number of worker goroutines
+type UserPopulation struct {
+	APIEndpoint string
+	UserCount   int
+	WorkerCount int
+	UserChannel chan int
+	WaitGroup   sync.WaitGroup
+	StartTime   time.Time
+}
 
-	var wg sync.WaitGroup
-	userChannel := make(chan int)
-	startTime := time.Now()
+func NewUserPopulation(apiURL string, userCount, workerCount int) *UserPopulation {
+	return &UserPopulation{
+		APIEndpoint: apiURL,
+		UserCount:   userCount,
+		WorkerCount: workerCount,
+		UserChannel: make(chan int),
+	}
+}
 
-	for i := 0; i < workerCount; i++ {
-		wg.Add(1)
-		go worker(apiEp, userChannel, &wg)
+func (up *UserPopulation) Start() {
+	up.StartTime = time.Now()
+	for i := 0; i < up.WorkerCount; i++ {
+		up.WaitGroup.Add(1)
+		go up.worker()
 	}
 
-	for i := 0; i < userCount; i++ {
-		userChannel <- i
+	for i := 0; i < up.UserCount; i++ {
+		up.UserChannel <- i
 	}
 
-	close(userChannel)
-	wg.Wait()
+	close(up.UserChannel)
+	up.WaitGroup.Wait()
 
-	elapsedTime := time.Since(startTime)
+	elapsedTime := time.Since(up.StartTime)
 	elapsedHours := int(elapsedTime.Hours())
 	elapsedMinutes := int(elapsedTime.Minutes()) % 60
 	elapsedSeconds := int(elapsedTime.Seconds()) % 60
@@ -44,11 +54,11 @@ func main() {
 	fmt.Printf("\nFinished in %02d:%02d:%02d\n", elapsedHours, elapsedMinutes, elapsedSeconds)
 }
 
-func worker(apiEp string, userChannel <-chan int, wg *sync.WaitGroup) {
-	apiUrl := fmt.Sprintf("%s/user", apiEp)
-	defer wg.Done()
+func (up *UserPopulation) worker() {
+	apiURL := fmt.Sprintf("%s/user", up.APIEndpoint)
+	defer up.WaitGroup.Done()
 
-	for userCount := range userChannel {
+	for userCount := range up.UserChannel {
 		newUser := generateRandomUser()
 		jsonData, err := json.Marshal(newUser)
 		if err != nil {
@@ -56,7 +66,7 @@ func worker(apiEp string, userChannel <-chan int, wg *sync.WaitGroup) {
 			return
 		}
 
-		resp, err := http.Post(apiUrl, "application/json", bytes.NewBuffer(jsonData))
+		resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(jsonData))
 		if err != nil {
 			fmt.Println("Error sending HTTP request:", err)
 			return
@@ -78,8 +88,25 @@ func worker(apiEp string, userChannel <-chan int, wg *sync.WaitGroup) {
 				return
 			}
 			userID := extractUserID(message)
-			makeAddCardRequests(apiEp, userID)
+			up.makeAddCardRequests(userID)
 			fmt.Printf("User #%d created successfully with ID: %d\n", userCount+1, userID)
+		}
+	}
+}
+
+func (up *UserPopulation) makeAddCardRequests(userID int) {
+	apiURL := fmt.Sprintf("%s/card/%d", up.APIEndpoint, userID)
+
+	for i := 0; i < 3; i++ {
+		resp, err := http.Post(apiURL, "", nil) // No request body needed
+		if err != nil {
+			fmt.Println("Error sending HTTP request:", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("Failed to create card #%d for user #%d, status code: %d\n", i+1, userID, resp.StatusCode)
 		}
 	}
 }
@@ -102,19 +129,11 @@ func extractUserID(message string) int {
 	return userID
 }
 
-func makeAddCardRequests(apiEp string, userID int) {
-	apiUrl := fmt.Sprintf("%s/card/%d", apiEp, userID)
+func main() {
+	apiURL := getenvs.GetEnvString("APIURL", "http://localhost:8080")
+	userCount, _ := getenvs.GetEnvInt("USERCOUNT", 10000)
+	workerCount, _ := getenvs.GetEnvInt("WORKERCOUNT", 4)
 
-	for i := 0; i < 3; i++ {
-		resp, err := http.Post(apiUrl, "", nil) // No request body needed
-		if err != nil {
-			fmt.Println("Error sending HTTP request:", err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			fmt.Printf("Failed to create card #%d for user #%d, status code: %d\n", i+1, userID, resp.StatusCode)
-		}
-	}
+	userPopulation := NewUserPopulation(apiURL, userCount, workerCount)
+	userPopulation.Start()
 }
